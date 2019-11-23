60bebde3aa691da3c42f60c7b92d09af638b3e73
arslan-13 <57611878+arslan-13@users.noreply.github.com>
2019-11-23 16:49:31 +0500

    Add project files.

---
+++ DataBase/POSDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBase
+{
+    public class POSDbContext : DbContext
+    {
+        public POSDbContext(DbContextOptions<POSDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> tblcategories { get; set; }
+        public DbSet<Category> tblcategories { get; set; }
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+
+        public virtual Vendor Vendor { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
