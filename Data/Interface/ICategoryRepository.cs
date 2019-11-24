741d7c999e5d7cad2fb4da32a619a7be04efb42a
arslan-13 <57611878+arslan-13@users.noreply.github.com>
2019-11-24 22:39:55 +0500

    Category init

---
+++ Data/Interface/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interface
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllCategory();
+        Task<Category> GetVendorByID(int ID);
+
+        void Add(Category category);
+        void Edit(Category category);
+        void Delete(Category category);
+        Task Save();
+    }
+}
---
+++ Data/Repository/CategoryRepository.cs
@@ -0,0 +1,51 @@
+﻿using Data.Interface;
+using DataBase;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly POSDbContext cntx;
+
+        public CategoryRepository(POSDbContext pOSDbContext)
+        {
+            cntx = pOSDbContext;
+        }
+
+        public void Add(Category category)
+        {
+            cntx.tblcategories.Add(category);
+        }
+
+        public void Delete(Category category)
+        {
+            cntx.tblcategories.Remove(category);
+        }
+
+        public void Edit(Category category)
+        {
+            cntx.tblcategories.Update(category);
+        }
+
+        public async Task<IEnumerable<Category>> GetAllCategory()
+        {
+            return await cntx.tblcategories.ToListAsync();
+        }
+
+        public async Task<Category> GetVendorByID(int ID)
+        {
+            return await cntx.tblcategories.FindAsync(ID);
+        }
+
+        public async Task Save()
+        {
+            await cntx.SaveChangesAsync();
+        }
+    }
+}
